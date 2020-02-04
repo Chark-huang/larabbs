b9f028cf30562bb5d0f893c20d2d52a76ada4b13
Chark <15814277028@163.com>
2020-02-04 09:29:51 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://www.baidu.com" target="_blank">Chark</a> 设计和编码
+        </p>
+        <p class="float-right"><a href="">联系我们</a></p>
+    </div>
+</footer>
