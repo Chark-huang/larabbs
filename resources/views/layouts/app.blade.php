b83dd4b2b05010f46ecf44c9657bf4caa5e6fb2a
Chark <15814277028@163.com>
2020-02-08 07:45:42 +0800

    用户个人页面原型

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
      <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('titel', 'LaraBBS')</title>
+    <title>@yield('title', 'LaraBBS')</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
