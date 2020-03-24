ee51ccb9c57f5aaa79299f8c250467fe78607e2f
Chark <15814277028@163.com>
2020-03-24 15:15:10 +0800

    WYSIWYG 编辑器

--- public/js/app.js
+++ public/js/app.js
@@ -49485,8 +49485,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Phpstudy\phpstudy_pro\WWW\larabbs\larabbs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Phpstudy\phpstudy_pro\WWW\larabbs\larabbs\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ruma\Code\larabbs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ruma\Code\larabbs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,22 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <!-- CSRF Token -->
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS')</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    @yield('styles')
 </head>
+
 <body>
 
     <div id="app" class="">
@@ -31,5 +35,8 @@
 
     {{--<!-- Script -->--}}
     <script src="{{ mix('js/app.js') }}"></script>
+
+    @yield('scripts')
 </body>
+
 </html>
