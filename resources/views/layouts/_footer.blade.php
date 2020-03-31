4592ccca5b30d79666ea20c994b9c80a105370e9
Chark <15814277028@163.com>
2020-03-31 18:03:15 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
         <p class="float-left">
             由 <a href="http://www.baidu.com" target="_blank">Chark</a> 设计和编码
         </p>
-        <p class="float-right"><a href="">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
